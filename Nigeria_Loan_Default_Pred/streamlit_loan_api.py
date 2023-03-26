07c5c1f18046cbe7a23dbf41506d0cd8bea7c52e
VICTORIA OKESIPE <vokesipe888@stu.ui.edu.ng>
2023-03-26 14:02:34 +0100

    Update streamlit_loan_api.py

--- Nigeria_Loan_Default_Pred/streamlit_loan_api.py
+++ Nigeria_Loan_Default_Pred/streamlit_loan_api.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from xgboost import XGBClassifier
-import sklearn
+import scikit-learn as sklearn 
 from sklearn.preprocessing import StandardScaler
 
 #Loading up the classifier model we created
