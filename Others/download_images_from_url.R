008cc61e6465900385ebfbd002a03389f366d0e3
VICTORIA OKESIPE <vokesipe888@stu.ui.edu.ng>
2023-07-19 17:10:42 +0100

    Update download_images_from_url.R

--- Others/download_images_from_url.R
+++ Others/download_images_from_url.R
@@ -1,6 +1,6 @@
 # Task -- Downloading, Saving, and Renaming Images downloaded from URL
 
-# This is an R code that can teach you how to perform 
+# This is an R code that can teach you how to perform the task 
 
 # loading required libraries and the data
 
