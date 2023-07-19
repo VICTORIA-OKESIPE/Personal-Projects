b2c695ccb7c14d05c2192696fa96ff1b1239bb45
VICTORIA OKESIPE <vokesipe888@stu.ui.edu.ng>
2023-07-19 17:06:32 +0100

    Update download_images_from_url.R

--- Others/download_images_from_url.R
+++ Others/download_images_from_url.R
@@ -1,6 +1,6 @@
-# Task -- Downloading, Saving and Renaming Images downloaded from URL
+# Task -- Downloading, Saving, and Renaming Images downloaded from URL
 
-# This is
+# This is a 
 
 # loading required libraries and the data
 
